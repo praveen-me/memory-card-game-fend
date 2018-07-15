c880f67fde0234d691c81e185ec1eec5a1ca9b50
praveen-me <praveenkrsaini0@gmail.com>
2018-07-15 05:49:45 +0530

    Add flip and matching functionality to cards, Add jQuery to the project

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 /*
- * Create a list that holds all of your cards
+ * App's JavaScript code
  */
 
+const cardList = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
+let started = false;
+let openCards = [];
+let moves = 0;
+let timeCount = 0;
+let solvedCount = 0;
+let timerPtr;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
+    let currentIndex = array.length, temporaryValue, randomIndex;
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -21,18 +21,103 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
+// get class value from card DOM
+function getClassFromCard(card){
+    return card[0].firstChild.className;
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// check open cards when count = 2
+function checkOpenCards(){
+    if (getClassFromCard(openCards[0]) === getClassFromCard(openCards[1])){
+        solvedCount++;
+        openCards.forEach(function(card){
+            card.animateCss('tada', function(){
+                card.toggleClass("open show match");
+            });
+        });
+    } else {
+        openCards.forEach(function(card){
+            card.animateCss('shake', function(){
+                card.toggleClass("open show");
+            });
+        });
+    }
+    openCards = [];
+}
+
+// event handler for when the card is clicked
+function cardClick(event){
+    // check opened or matched card
+    let classes = $(this).attr("class");
+    if (classes.search('open') * classes.search('match') !== 1){
+        // both should be -1
+        return;
+    }
+    // start game if needed
+    if (!started) {
+        started = true;
+        timeCount = 0;
+    }
+    // cards can be flipped
+    if (openCards.length < 2){
+        $(this).toggleClass("open show");
+        openCards.push($(this));
+    }
+    // check if cards match
+    if (openCards.length === 2){
+        checkOpenCards();
+    }
+}
+
+// create individual card element
+function createCard(cardClass){
+    $("ul.deck").append(`<li class="card"><i class="fa ${cardClass}"></i></li>`);
+}
+
+// populate cards in DOM
+function populateCards(){
+    shuffle(cardList.concat(cardList)).forEach(createCard);
+}
+
+// init game
+function initGame(){
+    populateCards();
+    $(".card").click(cardClick);
+}
+
+// things done after DOM is loaded for the first time
+$(document).ready(function(){
+    initGame();
+});
+
+
+//source https://github.com/daneden/animate.css
+$.fn.extend({
+    animateCss: function(animationName, callback) {
+      var animationEnd = (function(el) {
+        var animations = {
+          animation: 'animationend',
+          OAnimation: 'oAnimationEnd',
+          MozAnimation: 'mozAnimationEnd',
+          WebkitAnimation: 'webkitAnimationEnd',
+        };
+  
+        for (var t in animations) {
+          if (el.style[t] !== undefined) {
+            return animations[t];
+          }
+        }
+      })(document.createElement('div'));
+  
+      this.addClass('animated ' + animationName).one(animationEnd, function() {
+        $(this).removeClass('animated ' + animationName);
+  
+        if (typeof callback === 'function') callback();
+      });
+  
+      return this;
+    },
+});
