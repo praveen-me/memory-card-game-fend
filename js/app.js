6c4e3d3c3ffa58fb7c4bf171f5b127b01da6c39c
praveen-me <praveenkrsaini0@gmail.com>
2018-07-16 08:06:15 +0530

    Add functionality like incrementing move, reducing star, solvindg count number

--- js/app.js
+++ js/app.js
@@ -46,6 +46,26 @@ function checkOpenCards(){
         });
     }
     openCards = [];
+    incrementMove();
+    if (solvedCount === 8){
+        endGame();
+    }
+}
+
+// starts the timer
+function startTimer(){
+    timeCount += 1;
+    $("#timer").html(timeCount);
+    timerPtr = setTimeout(startTimer, 1000);
+}
+
+// increment move count
+function incrementMove(){
+    moves += 1;
+    $("#moves").html(moves);
+    if (moves === 14 || moves === 20){
+        reduceStar();
+    }
 }
 
 // event handler for when the card is clicked
@@ -60,6 +80,7 @@ function cardClick(event){
     if (!started) {
         started = true;
         timeCount = 0;
+        timerPtr = setTimeout(startTimer, 1000);
     }
     // cards can be flipped
     if (openCards.length < 2){
@@ -82,42 +103,75 @@ function populateCards(){
     shuffle(cardList.concat(cardList)).forEach(createCard);
 }
 
+// reset game
+function resetGame(){
+    $("ul.deck").html("");
+    $(".stars").html("");
+    moves = -1;
+    incrementMove();
+    started = false;
+    openCards = [];
+    timeCount = 0;
+    solvedCount = 0;
+    clearTimeout(timerPtr);
+    $("#timer").html(0);
+    // re-setup game
+    initGame();
+}
+
+// runs when game has been won
+function endGame(){
+    // stop timer
+    clearTimeout(timerPtr);
+    // show prompt
+    let stars = $(".fa-star").length;
+    vex.dialog.confirm({
+        message: `Congrats! You just won the game in ${timeCount} seconds with ${stars}/3 star rating. Do you want to play again?`,
+        callback: function(value){
+            if (value){
+                resetGame();
+            }
+        }
+    });
+}
+
+// initialize stars display
+function initStars(){
+    for (let i=0; i<3; i++){
+        $(".stars").append(`<li><i class="fa fa-star"></i></li>`);
+    }
+}
+
+// reduce star rating
+function reduceStar(){
+    let stars = $(".fa-star");
+    $(stars[stars.length-1]).toggleClass("fa-star fa-star-o");
+}
+
 // init game
 function initGame(){
     populateCards();
+    initStars();
     $(".card").click(cardClick);
 }
 
 // things done after DOM is loaded for the first time
 $(document).ready(function(){
     initGame();
+    $("#restart").click(resetGame);
 });
 
-
-//source https://github.com/daneden/animate.css
+// load animateCss
+// taken from https://github.com/daneden/animate.css/#usage
 $.fn.extend({
-    animateCss: function(animationName, callback) {
-      var animationEnd = (function(el) {
-        var animations = {
-          animation: 'animationend',
-          OAnimation: 'oAnimationEnd',
-          MozAnimation: 'mozAnimationEnd',
-          WebkitAnimation: 'webkitAnimationEnd',
-        };
-  
-        for (var t in animations) {
-          if (el.style[t] !== undefined) {
-            return animations[t];
-          }
-        }
-      })(document.createElement('div'));
-  
-      this.addClass('animated ' + animationName).one(animationEnd, function() {
-        $(this).removeClass('animated ' + animationName);
-  
-        if (typeof callback === 'function') callback();
-      });
-  
-      return this;
-    },
+    animateCss: function (animationName, callback) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function () {
+            $(this).removeClass('animated ' + animationName);
+            if (callback) {
+                callback();
+            }
+        });
+        return this;
+    }
 });
