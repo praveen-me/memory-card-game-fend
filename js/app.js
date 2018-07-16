70cab648bff35057584af3c86552639158d22cb9
praveen-me <praveenkrsaini0@gmail.com>
2018-07-17 03:43:38 +0530

    Add levels to the game

--- js/app.js
+++ js/app.js
@@ -1,19 +1,36 @@
 /*
- * App's JavaScript code
- */
+ * Create a list that holds all of your cards
+*/
 
-const cardList = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
+// const cardList = ['fa-repeat', 'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle'];
+
+const cardListForEasy = ['fa-repeat', 'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube'];
+
+const cardListForMedium = ['fa-repeat', 'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle'];
+
+const cardListForHard = ['fa-repeat', 'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-adjust' , 'fa-tint']; 
+
 let started = false;
 let openCards = [];
-let moves = 0;
 let timeCount = 0;
+let moves = 0;
 let solvedCount = 0;
-let timerPtr;
+let timePtr;
+let clickedEasy = false;
+let clickedMedium = false;
+let clickedHard = false;
 
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+*/
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -21,157 +38,317 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
+
     return array;
 }
 
-// get class value from card DOM
-function getClassFromCard(card){
+
+// make card 
+function makeCard(cardClass){
+    $('ul.deck').append(`<li class="card"><i class="fa ${cardClass}"></i></li>`);
+}
+
+//populated these card in DOM and shuffle them
+function populateCardDeck (cardLists){
+    shuffle(cardLists.concat(cardLists)).forEach(makeCard);
+}
+
+//take card class 
+function getClassFromCard(card) {
     return card[0].firstChild.className;
 }
 
-// check open cards when count = 2
-function checkOpenCards(){
-    if (getClassFromCard(openCards[0]) === getClassFromCard(openCards[1])){
+//check card for second click and compare them and perform basics functionalities like increamenitng move etc.
+function checkCards(){
+    if(getClassFromCard(openCards[0]) === getClassFromCard(openCards[1])){
         solvedCount++;
-        openCards.forEach(function(card){
-            card.animateCss('tada', function(){
-                card.toggleClass("open show match");
+        openCards.forEach(function (card){
+            card.animateCss('jello', function (){
+                card.toggleClass('open show match');
             });
         });
     } else {
-        openCards.forEach(function(card){
-            card.animateCss('shake', function(){
-                card.toggleClass("open show");
+        openCards.forEach(function (card){
+            card.animateCss('rubberBand', function (){
+                card.toggleClass('open show');
             });
         });
     }
+
     openCards = [];
     incrementMove();
-    if (solvedCount === 8){
-        endGame();
+
+    // End game for particular level
+    if(clickedEasy === true) {
+        if(solvedCount === 6) {
+            endGame();
+        }
+    } else if(clickedMedium === true) {
+        if(solvedCount === 8) {
+            endGame();
+        }
+    } else if(clickedHard === true) {
+        if(solvedCount === 10) {
+            endGame();
+        }
     }
 }
 
-// starts the timer
-function startTimer(){
-    timeCount += 1;
-    $("#timer").html(timeCount);
-    timerPtr = setTimeout(startTimer, 1000);
-}
-
-// increment move count
+//function for move incrementing
 function incrementMove(){
     moves += 1;
-    $("#moves").html(moves);
-    if (moves === 14 || moves === 20){
-        reduceStar();
+    $('#moves').html(moves);
+
+    // Reducing Star according to particular level
+    if(clickedEasy === true) {
+        if(moves === 10 || moves === 15) {
+            reduceStar();
+        }
+    } else if(clickedMedium === true) {
+        if(moves === 15 || moveBy === 20) {
+            reduceStar();
+        }
+    } else if(clickedHard === true) {
+        if(moves === 17 || moves === 22) {
+            reduceStar();
+        }
     }
 }
 
-// event handler for when the card is clicked
-function cardClick(event){
-    // check opened or matched card
-    let classes = $(this).attr("class");
-    if (classes.search('open') * classes.search('match') !== 1){
-        // both should be -1
+
+//funtion for initing stars when the page loaded
+function initStars(){
+    for(let i = 0; i < 3; i++){
+        $('.stars').append('<li><i class="fa fa-star"></i></li>');
+    }
+}
+
+//function for reducing stars according to the moves
+function reduceStar(){
+    let star = $('.fa-star');
+    $(star[star.length - 1]).toggleClass('fa-star fa-star-o');
+}
+
+//function for start timer
+function startTimer() {
+    $('#timer').html('');
+    timeCount += 1;
+    $('#timer').html(timeCount);
+    timePtr = setTimeout(startTimer, 1000);
+}
+
+//function occur when the event triggered
+function cardClicked(event){
+    //check for open and match card
+    let classes = $(this).attr('class');
+
+    if(classes.search('open') * classes.search('match') !== 1){
+        //both should be -1
         return;
     }
-    // start game if needed
-    if (!started) {
+
+    //start game when needed
+    if(!started){
         started = true;
         timeCount = 0;
-        timerPtr = setTimeout(startTimer, 1000);
+        timePtr = setTimeout(startTimer, 1000);
     }
-    // cards can be flipped
-    if (openCards.length < 2){
-        $(this).toggleClass("open show");
+
+    //cards can be flipped
+    if(openCards.length < 2){
+        $(this).toggleClass('open show');
         openCards.push($(this));
     }
-    // check if cards match
-    if (openCards.length === 2){
-        checkOpenCards();
+
+    //cards can be matched
+    if(openCards.length === 2){
+        checkCards();
     }
+
 }
 
-// create individual card element
-function createCard(cardClass){
-    $("ul.deck").append(`<li class="card"><i class="fa ${cardClass}"></i></li>`);
+
+//function for  end game prompt
+function endGamePrompt(){
+    let stars = $('.fa-star');
+
+    $('.end-modal').empty();
+    $('.end-modal').addClass('animated zoomIn')
+    $('.end-modal').css('display', 'flex');   
+
+    $('.end-modal').append(`
+    <p>CONGRATS!! You got ${stars.length}/3 rating and completed in ${timeCount} seconds with ${moves} moves. <br>
+    Do you want to play it again?</p>`);
+
+    $('.end-modal').append(`<button class="start-yes modal-button" id="started">Yes</button>
+    <button class="start-no modal-button" id="not-started">No</button>`);
+
+    $('.end-modal').append(`<button id="saveProgressAskButton"> Save Your Progress </button>`);
 }
 
-// populate cards in DOM
-function populateCards(){
-    shuffle(cardList.concat(cardList)).forEach(createCard);
+//function for if user clcik on No Button in End Game Modal or basically function for say no prompt
+function sayNoPrompt() {
+    $('.end-modal').css('display', 'none');
+    $('.sayNo-modal').addClass('animated slideInLeft');
+    $('.sayNo-modal').css('display', 'block');
+
+    $('#closeSayNoModal').click(function(){
+        $('.sayNo-modal').css('display', 'none');
+    })
 }
 
-// reset game
+
+//function for endgame
+function endGame(){
+   
+    endGamePrompt(); // calling endgame
+
+    let startYes = document.getElementById('started');
+
+    clearTimeout(timePtr); // stop the time
+    
+    //event triggered on Yes button and restart game again
+    startYes.addEventListener('click', function () {
+        resetGame();
+        $('.end-modal').css('display', 'none');
+    });
+    
+    //If user don't want to play further
+   $('#not-started').click(sayNoPrompt);
+
+
+
+}
+
+// reset game prompt
+function resetGamePrompt(){
+    $('.reset-modal').css('display', 'flex');
+    $('.reset-modal').addClass('animated slideInRight');
+
+    $('.reset-yes').click(function (){
+        resetGame();
+        $('.reset-modal').css('display', 'none');
+    });
+
+    $('.reset-no').click(function(){
+        $('.reset-modal').css('display', 'none');
+    })
+
+}
+
+// reset the game
 function resetGame(){
-    $("ul.deck").html("");
-    $(".stars").html("");
+    $('ul.deck').html('');
+    $('.stars').html('');
     moves = -1;
     incrementMove();
     started = false;
-    openCards = [];
     timeCount = 0;
+    openCards = [];
     solvedCount = 0;
-    clearTimeout(timerPtr);
-    $("#timer").html(0);
-    // re-setup game
+    clearTimeout(timePtr);
+    $('#timer').html(0);
+    //setup the game again
     initGame();
 }
 
-// runs when game has been won
-function endGame(){
-    // stop timer
-    clearTimeout(timerPtr);
-    // show prompt
-    let stars = $(".fa-star").length;
-    vex.dialog.confirm({
-        message: `Congrats! You just won the game in ${timeCount} seconds with ${stars}/3 star rating. Do you want to play again?`,
-        callback: function(value){
-            if (value){
-                resetGame();
-            }
-        }
-    });
-}
-
-// initialize stars display
-function initStars(){
-    for (let i=0; i<3; i++){
-        $(".stars").append(`<li><i class="fa fa-star"></i></li>`);
-    }
-}
-
-// reduce star rating
-function reduceStar(){
-    let stars = $(".fa-star");
-    $(stars[stars.length-1]).toggleClass("fa-star fa-star-o");
-}
-
-// init game
+//initialize the game
 function initGame(){
-    populateCards();
+
+    // Initialize game accoring to the level
+    if(clickedEasy === true){
+        populateCardDeck(cardListForEasy);
+    } else if(clickedMedium === true){
+        populateCardDeck(cardListForMedium);
+    } else if(clickedHard === true){
+        populateCardDeck(cardListForHard);
+        $('.card').css({'height': '100px',
+        'width':'100px'});
+
+        // Making Card deck responsive for hard level
+        if ($(window).width() <= 480) {
+            $('.card').css({'height': '50px','width':'50px'});
+        } else if($(window).width() >= 481 && $(window).width() <= 768){
+            $('.card').css({'height': '75px','width':'75px'});
+        }
+
+    }
+
     initStars();
-    $(".card").click(cardClick);
+    $('.card').click(cardClicked);
 }
 
-// things done after DOM is loaded for the first time
-$(document).ready(function(){
-    initGame();
-    $("#restart").click(resetGame);
+// levels modal 
+function levelModal() {
+    $('.levels').css('display','flex');
+    $('.levels').addClass('animated bounceIn');
+
+    // When Player Click on Easy Button
+    $('#easy').click(function (){
+        clickedEasy = true;
+        $('.deck').css('display', 'flex');
+
+        initGame();
+
+        $('.levels').css('display','none');
+    });
+
+    // When Player Click on Medium Button
+    $('#medium').click(function (){
+        clickedMedium = true;
+        $('.deck').css('display', 'flex');
+
+        initGame();
+
+        $('.levels').css('display','none');
+    });
+
+    // When Player Click on Hard Button
+    $('#hard').click(function (){
+        clickedHard = true;
+        $('.deck').css('display', 'flex');
+        
+        initGame();
+
+        $('.levels').css('display','none');
+    });
+    
+}
+
+
+//When the DOM is ready to work
+$(document).ready(function (){
+    levelModal();
+    
+    $('#restart').click(resetGamePrompt);
 });
 
-// load animateCss
-// taken from https://github.com/daneden/animate.css/#usage
+
+//source https://github.com/daneden/animate.css
 $.fn.extend({
-    animateCss: function (animationName, callback) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function () {
-            $(this).removeClass('animated ' + animationName);
-            if (callback) {
-                callback();
-            }
-        });
-        return this;
-    }
+    animateCss: function(animationName, callback) {
+      var animationEnd = (function(el) {
+        var animations = {
+          animation: 'animationend',
+          OAnimation: 'oAnimationEnd',
+          MozAnimation: 'mozAnimationEnd',
+          WebkitAnimation: 'webkitAnimationEnd',
+        };
+  
+        for (var t in animations) {
+          if (el.style[t] !== undefined) {
+            return animations[t];
+          }
+        }
+      })(document.createElement('div'));
+  
+      this.addClass('animated ' + animationName).one(animationEnd, function() {
+        $(this).removeClass('animated ' + animationName);
+  
+        if (typeof callback === 'function') callback();
+      });
+  
+      return this;
+    },
 });
+
